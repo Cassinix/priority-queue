70f19090dc1d7ff7237e93aca39348256543a14b
Cassinix <ninicassini@gmail.com>
2019-03-22 22:05:54 +0300

    add: some node.js methods

--- src/node.js
+++ src/node.js
@@ -1,18 +1,35 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
-	appendChild(node) {
-
+    appendChild(node) {
+        if (this.left === null) {
+  			node.parent = this;
+            this.left = node;
+        }
+       else if (this.right === null) {
+  			node.parent = this;
+            this.right = node;
+        }
 	}
-
+	
 	removeChild(node) {
-
+		node.parent = null;
+    	if (this.left === node) {
+     		this.left = null;
+		} 
+		else {
+      		this.right = null;
+    	}
 	}
 
 	remove() {
-
+		
 	}
 
 	swapWithParent() {

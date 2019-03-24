ec6f1afc2b11f47be7eff34910b27692822bccc3
Cassinix <ninicassini@gmail.com>
2019-03-24 06:22:32 +0300

    fead: implement swapWithParent()

--- src/node.js
+++ src/node.js
@@ -7,26 +7,27 @@ class Node {
 		this.right = null;
 	}
 
-    appendChild(node) {
-        if (this.left === null) {
-  			node.parent = this;
-            this.left = node;
-        }
-       else if (this.right === null) {
-  			node.parent = this;
-            this.right = node;
-        }
-	}
-	
-	removeChild(node) {
-		node.parent = null;
-    	if (this.left === node) {
-     		this.left = null;
-		} 
-		else if (this.right === node) { 
-      		this.right = null;
+	appendChild(node) {
+		if (this.left === null) {
+			node.parent = this;
+			this.left = node;
 		}
-		else throw 'Error: passed node is not a child of this node';
+		else if (this.right === null) {
+			node.parent = this;
+			this.right = node;
+		}
+	}
+
+	removeChild(node) {
+		if (node === this.left) {
+			this.left = null;
+			node.parent = null;
+		}
+		else if (node === this.right) {
+			this.right = null;
+			node.parent = null;
+		}
+		else throw new Error('Passed node is not a child of this node');
 	}
 
 	remove() {
@@ -36,27 +37,57 @@ class Node {
 	}
 	//Hewson we have a problem
 	swapWithParent() {
-		//поменять сына деду
-		//поменять родителей
-		//поменять детей
-		if (this.parent = null) {
-			return;
-		}
-		else if (this.parent.left = this) {
-			this.right = this.parent.right;
-			this.left = this.parent;
+		const updateNodes = () => {
+			const parentOfParent = this.parent.parent;
+			const parentOfNode = this.parent;
+			let brotherOfNode;
+			const thisNode = this;
+			const leftChild = this.left;
+			const rightChild = this.right;
+
+			//новые дети this и их родители
+			if (this.parent.left === this && this.parent.right !== null) {
+				brotherOfNode = this.parent.right;
+				brotherOfNode.parent = this;
+				this.right = brotherOfNode;
+				this.left = this.parent;//dunno
+
+			}
+			if (this.parent.right === this) {
+				brotherOfNode = this.parent.left;
+				brotherOfNode.parent = this;
+				this.left = brotherOfNode;
+				this.right = this.parent;//dunno
+			}
+
+			//ребенок деда
+			if (this.parent.parent !== null) {
+				if (this.parent.parent.left === this.parent) {
+					parentOfParent.left = this;
+				} else if (this.parent.parent.right === this.parent) {
+					parentOfParent.right = this;
+				}
+			}
+
+
+			//родители детей
+			if (leftChild !== null) {
+				leftChild.parent = this.parent;
+			}
+			if (rightChild !== null) {
+				rightChild.parent = this.parent;
+			}
+
+			//возврат значений
+			this.parent.left = leftChild;
+			this.parent.right = rightChild;
+			const acc = this.parent.parent;
 			this.parent.parent = this;
-			this.parent = this.parent.parent;
-			this.parent.right = this.right;
-			this.parent.left = this.left;
-		}
-		else if (this.parent.right = this) {
-			this.right = this.parent;
-			this.left = this.parent.left;
-			this.parent.parent = this;
-			this.parent = this.parent.parent;
-			this.parent.right = this.right;
-			this.parent.left = this.left;
+			this.parent = acc;
+		};
+
+		if (this.parent !== null) {
+			updateNodes();
 		}
 	}
 }
